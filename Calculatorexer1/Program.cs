5bac8ad6ce6faebefae3e97a30f7d536b8ec4dc9
taichi88 <nikoloz.gabeskiria@gmail.com>
2024-12-03 14:32:49 +0400

    Added whileloop

--- Calculatorexer1/Program.cs
+++ Calculatorexer1/Program.cs
@@ -6,37 +6,55 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Enter the one of the Operation Symbol \n+ \n- \n* \n/ ");
-        string operation = Console.ReadLine();
-        if (operation != "+" && operation != "-" && operation != "*" && operation != "/")
-
-            Console.WriteLine("Error: invalid operations. ");
-
-        else
+        Console.WriteLine("Calculator has started ");
+        bool continue1 = true;
+        while (continue1)
+                
         {
+            
+            Console.WriteLine("Enter the one of the Operation Symbol \n+ \n- \n* \n/ ");
+            string operation = Console.ReadLine();
+            if (operation != "+" && operation != "-" && operation != "*" && operation != "/")
 
-            Console.WriteLine("Enter First Number");
-            int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Error: invalid operations. ");
 
-            Console.WriteLine("Enter Second Number");
-            int num2 = int.Parse(Console.ReadLine());
-
-            if (operation == "+")
-                Console.WriteLine("sum is " + (num1 + num2));
-            else if (operation == "-")
-                Console.WriteLine("sum is " + (num1 - num2));
-            else if (operation == "*")
-                Console.WriteLine("sum is " + (num1 * num2));
-            else if (operation == "/")
+            else
             {
-                if (num2 != 0)
-                    Console.WriteLine("sum is " + num1 / (double)num2);
-                else
-                    Console.WriteLine("Error: Divison by zero is not allowed");
+
+                Console.WriteLine("Enter First Number");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Second Number");
+                int num2 = int.Parse(Console.ReadLine());
+
+                if (operation == "+")
+                    Console.WriteLine("sum is " + (num1 + num2));
+                else if (operation == "-")
+                    Console.WriteLine("sum is " + (num1 - num2));
+                else if (operation == "*")
+                    Console.WriteLine("sum is " + (num1 * num2));
+                else if (operation == "/")
+                {
+                    if (num2 != 0)
+                        Console.WriteLine("sum is " + num1 / (double)num2);
+                    else
+                        Console.WriteLine("Error: Divison by zero is not allowed");
+
+                }
+                Console.WriteLine(" The first number is {0}, The second number is {1},", num1, num2);
+                Console.WriteLine("If you would like to continue press 'y' ");
+                string askUser = Console.ReadLine();
+                if (askUser != "y")
+                {
+                    
+                    continue1 = false;
+                }
+                Console.Clear();
+
 
             }
-            Console.WriteLine(" The first number is {0}, The second number is {1},", num1, num2);
-
         }
+        Console.Clear();
+        Console.WriteLine("Thank you for using Calculator. See you next time");
     }
 }
